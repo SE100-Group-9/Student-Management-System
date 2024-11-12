43628b26e05545d7944cae672a787326c7f2ecbd
DangThiNgocMinh <ngocminh87049@gmail.com>
2024-11-12 23:34:06 +0700

    add components

---
+++ app/Views/components/exit_button.php
@@ -0,0 +1,41 @@
+<div class="exit-container">
+    <button class="button-text">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+        <path d="M12 15L15 12M15 12L12 9M15 12L4 12M4 17C4 17.9319 4 18.3978 4.15224 18.7654C4.35523 19.2554 4.74481 19.6448 5.23486 19.8478C5.6024 20 6.06812 20 7 20H16.8C17.9201 20 18.48 20 18.9078 19.782C19.2841 19.5902 19.5905 19.2844 19.7822 18.908C20.0002 18.4802 20 17.9201 20 16.8V7.19995C20 6.07985 20.0002 5.51986 19.7822 5.09204C19.5905 4.71572 19.2841 4.40973 18.9078 4.21799C18.48 4 17.9201 4 16.8 4H7C6.06812 4 5.60241 4 5.23486 4.15224C4.74481 4.35523 4.35523 4.74456 4.15224 5.23462C4 5.60216 4 6.0681 4 6.99999" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+        Thoát
+    </button>
+</div>
+
+<style>
+    .exit-container {
+        position: relative;
+        width: 100px;
+        display: flex;
+        align-items: center;
+    }
+
+    .button-text {
+        display: inline-flex;
+        align-items: center;
+        height: 40px;
+        padding: 10px;
+        gap: 10px;
+        border-radius: 10px;
+        border: none;
+        background-color: var(--Cerise-Red, #E14177);
+        color: var(--White, #FFF);
+        font-family: Inter, sans-serif;
+        font-size: 16px;
+        font-weight: 700;
+        line-height: normal;
+        cursor: pointer;
+    }
+
+    .svg {
+        position: absolute;
+        top: 50%;
+        left: 100px;
+        transform: translateY(-50%);     
+    }
+</style>
