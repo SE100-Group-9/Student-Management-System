46709cdbc14311c5766781c7ebe68f926c287914
DangThiNgocMinh <ngocminh87049@gmail.com>
2024-11-16 11:56:17 +0700

    Resolve conflict in Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,4 +8,12 @@ use CodeIgniter\Router\RouteCollection;
 
 $routes->get('/director/dashboard', 'DirectorController::dashboard');
 
+$routes->get('student/score', 'StudentController::score');
+$routes->get('student/profile', 'StudentController::profile');
+
+$routes->get('cashier/payment', 'CashierController::payment');
+$routes->get('cashier/extense', 'CashierController::extense');
+
 $routes->get('supervisor/fault', 'SupervisorController::fault');
+
+$routes->get('/', 'Home::index');
