3e27539c9e3bc8a298906247dea7aa5cb76ac911
Phluynhh <22520778@gm.uit.edu.vn>
2024-11-17 10:24:38 +0700

    add route in director

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,19 +6,27 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
+//  director
 $routes->get('/director/dashboard', 'DirectorController::dashboard');
+$routes->get('/director/news', 'DirectorController::news');
+$routes->get('director/statics/conduct', 'DirectorController::staticsConduct');
+$routes->get('director/statics/student', 'DirectorController::staticsStudent');
+$routes->get('director/statics/grade', 'DirectorController::staticsGrade');
 
+// student
 $routes->get('student/score', 'StudentController::score');
 $routes->get('student/profile', 'StudentController::profile');
 $routes->get('student/final_result', 'StudentController::final_result');
 
+// cashier
 $routes->get('cashier/payment/list', 'CashierController::list');
 $routes->get('cashier/extense', 'CashierController::extense');
 $routes->get('cashier/profile', 'CashierController::profile');
 $routes->get('cashier/payment/viewinfo', 'CashierController::viewinfo');
 
+// supervisor
 $routes->get('supervisor/fault', 'SupervisorController::fault');
 $routes->get('supervisor/profile', 'SupervisorController::profile');
 
+// login 
 $routes->get('/', 'Home::index');
-
--- app/Controllers/DirectorController.php
+++ app/Controllers/DirectorController.php
@@ -10,4 +10,24 @@ class DirectorController extends Controller
     {
         return view('director/dashboard');
     }
+
+    public function news()
+    {
+        return view('director/news');
+    }
+
+    public function staticsConduct()
+    {
+        return view('director/statics/conduct');
+    }
+
+    public function staticsGrade()
+    {
+        return view('director/statics/grade');
+    }
+
+    public function staticsStudent()
+    {
+        return view('director/statics/student');
+    }
 }
