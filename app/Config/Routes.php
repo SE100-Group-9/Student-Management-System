1912c464df85d6c7c1c9ab817763fc0ed7fc040a
DangThiNgocMinh <ngocminh87049@gmail.com>
2024-11-20 08:59:13 +0700

    Resolve merge conflict in app/Config/Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,11 +12,18 @@ $routes->get('/director/news', 'DirectorController::news');
 $routes->get('director/statics/conduct', 'DirectorController::staticsConduct');
 $routes->get('director/statics/student', 'DirectorController::staticsStudent');
 $routes->get('director/statics/grade', 'DirectorController::staticsGrade');
+$routes->get('director/student/add', 'DirectorController::studentAdd');
+$routes->get('director/student/update', 'DirectorController::studentUpdate');
+$routes->get('director/student/list', 'DirectorController::studentList');
+$routes->get('director/student/record', 'DirectorController::studentRecord');
+$routes->get('director/student/perserved', 'DirectorController::studentPerserved');
+$routes->get('director/student/payment', 'DirectorController::studentPayment');
 
 // student
 $routes->get('student/score', 'StudentController::score');
 $routes->get('student/profile', 'StudentController::profile');
 $routes->get('student/final_result', 'StudentController::final_result');
+$routes->get('student/fee_payment', 'StudentController::fee_payment');
 
 // cashier
 $routes->get('cashier/payment/list', 'CashierController::list');
@@ -24,6 +31,7 @@ $routes->get('cashier/extense', 'CashierController::extense');
 $routes->get('cashier/profile', 'CashierController::profile');
 $routes->get('cashier/payment/viewinfo', 'CashierController::viewinfo');
 $routes->get('cashier/payment/add', 'CashierController::add');
+$routes->get('cashier/payment/addextense', 'CashierController::addextense');
 
 // supervisor
 $routes->get('supervisor/fault', 'SupervisorController::fault');
