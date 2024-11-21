104f7801c647784e45de257cc1c2404400021df1
Phluynhh <22520778@gm.uit.edu.vn>
2024-11-22 01:59:04 +0700

    done 2 screens

---
+++ app/Views/components/tables/directorStudentPayment.php
@@ -0,0 +1,159 @@
+<div class="table-container">
+    <table id="directorStaticsConduct">
+        <tr>
+            <th>Mã số học sinh</th>
+            <th>Họ tên</th>
+            <th>Thông tin liên hệ</th>
+            <th>Lớp học</th>
+            <th>Tình trạng</th>
+            <th>Tiền nợ</th>
+        </tr>
+        <tr>
+            <td>01</td>
+            <td>Lê Nguyễn Hoài Thương</td>
+            <td>lnht@gmail.com</td>
+            <td>11A1</td>
+            <td>
+                <?= view('components/status', ['status' => 'Đã thu']); ?>
+            </td>
+            <td>1.000.000</td>
+        </tr>
+        <tr>
+            <td>02</td>
+            <td>Đoàn Ngọc Hoàng</td>
+            <td>lnht@gmail.com</td>
+            <td>11A1</td>
+            <td>
+                <?= view('components/status', ['status' => 'Đã thu']); ?>
+            </td>
+            <td>1.000.000</td>
+        </tr>
+        <tr>
+            <td>03</td>
+            <td>Vũ Thị Oanh</td>
+            <td>lnht@gmail.com</td>
+            <td>11A1</td>
+            <td>
+                <?= view('components/status', ['status' => 'Đã thu']); ?>
+            </td>
+            <td>1.000.000</td>
+        </tr>
+        <tr>
+            <td>04</td>
+            <td>Phan Huỳnh Thành Khương</td>
+            <td>lnht@gmail.com</td>
+            <td>11A1</td>
+            <td>
+                <?= view('components/status', ['status' => 'Đã thu']); ?>
+            </td>
+            <td>1.000.000</td>
+        </tr>
+        <tr>
+            <td>05</td>
+            <td>Bùi Nhựt Tân</td>
+            <td>lnht@gmail.com</td>
+            <td>11A1</td>
+            <td>
+                <?= view('components/status', ['status' => 'Đã thu']); ?>
+            </td>
+            <td>1.000.000</td>
+        </tr>
+        <tr>
+            <td>06</td>
+            <td>Huỳnh Tuyết Nhi</td>
+            <td>lnht@gmail.com</td>
+            <td>11A1</td>
+            <td>
+                <?= view('components/status', ['status' => 'Đã thu']); ?>
+            </td>
+            <td>1.000.000</td>
+        </tr>
+        <tr>
+            <td>07</td>
+            <td>Bạch Nguyệt Quang</td>
+            <td>lnht@gmail.com</td>
+            <td>11A1</td>
+            <td>
+                <?= view('components/status', ['status' => 'Đã thu']); ?>
+            </td>
+            <td>1.000.000</td>
+        </tr>
+        <tr>
+            <td>08</td>
+            <td>Nguyễn Lê Trung</td>
+            <td>lnht@gmail.com</td>
+            <td>11A1</td>
+            <td>
+                <?= view('components/status', ['status' => 'Đã thu']); ?>
+            </td>
+            <td>1.000.000</td>
+        </tr>
+        <tr>
+            <td>09</td>
+            <td>Cao Long Nhựt</td>
+            <td>lnht@gmail.com</td>
+            <td>11A1</td>
+            <td>
+                <?= view('components/status', ['status' => 'Đã thu']); ?>
+            </td>
+            <td>1.000.000</td>
+        </tr>
+        <tr>
+            <td>10</td>
+            <td>Bùi Nhựt Tân</td>
+            <td>lnht@gmail.com</td>
+            <td>11A1</td>
+            <td>
+                <?= view('components/status', ['status' => 'Đã thu']); ?>
+            </td>
+            <td>1.000.000</td>
+        </tr>
+    </table>
+</div>
+
+<style>
+    .table-container {
+        display: block;
+        /* Đảm bảo container là block */
+        overflow-x: auto;
+        width: 100%;
+        height: 100%;
+    }
+
+    #directorStaticsConduct {
+        min-width: 100%;
+        font-family: "Inter";
+        border-collapse: collapse;
+        color: black;
+        height: 100%;
+    }
+
+    #directorStaticsConduct th {
+        padding: 10px;
+        text-align: left;
+        background-color: rgba(185, 185, 185, 0.50);
+        color: black;
+        white-space: nowrap;
+    }
+
+    #directorStaticsConduct td {
+        padding: 10px;
+        text-align: left;
+        color: black;
+        background-color: white;
+        border-top: 1px solid rgba(0, 60, 60, 0.50);
+        white-space: nowrap;
+    }
+
+    #directorStaticsConduct a {
+        text-decoration: none;
+    }
+
+    #directorStaticsConduct a:hover {
+        background-color: transparent;
+    }
+
+    #directorStaticsConduct svg {
+        cursor: pointer;
+    }
+</style>
---
+++ app/Views/director/student/payment.php
@@ -0,0 +1,102 @@
+<div class="studentpayment">
+    <div class="studentpayment-heading">
+        <?= view('components/heading') ?>
+    </div>
+    <div class="body">
+        <div class="body-left">
+            <?= view('components/sidebar_director') ?>
+        </div>
+        <div class="body-right">
+            Học tập / Học sinh / Bảo lưu
+            <div class="studentpayment-tools">
+                <div class="tool-search">
+                    <?= view('components/filter') ?>
+                    <?= view('components/searchbar') ?>
+                </div>
+                <div class="tool-add">
+                    <?= view('components/excel_export') ?>
+                </div>
+            </div>
+            <?= view('components/tables/directorStudentPayment') ?>
+        </div>
+    </div>
+</div>
+
+<style>
+    *,
+    *::before,
+    *::after {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    .studentpayment {
+        display: flex;
+        width: 100%;
+        height: 100%;
+        flex-direction: column;
+        align-items: flex-start;
+        background: #FFF;
+    }
+
+    .studentpayment-heading {
+        width: 100%;
+        height: 60px;
+        position: fixed;
+    }
+
+    .body {
+        display: flex;
+        align-items: flex-start;
+        flex: 1 0 0;
+        margin-top: 60px;
+        align-self: stretch;
+        background: var(--light-grey, #F9FAFB);
+        overflow: hidden;
+    }
+
+    .body-left {
+        height: 100%;
+        overflow-y: auto;
+    }
+
+    .body-right {
+        display: flex;
+        padding: 20px;
+        flex-direction: column;
+        align-items: flex-start;
+        gap: 20px;
+        flex: 1 0 0;
+        align-self: stretch;
+        color: #000;
+        font-family: Inter;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+        overflow-y: auto;
+    }
+
+    .studentpayment-tools {
+        display: flex;
+        width: 100%;
+        padding: 10px;
+        justify-content: space-between;
+        align-items: flex-start;
+        border-radius: 10px;
+        background: #FFF;
+    }
+
+    .tool-search {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+    }
+
+    .tool-add {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+    }
+</style>
