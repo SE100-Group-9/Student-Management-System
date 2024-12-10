d392bebd15c1c69739a42b201c4c4599058e9217
Phluynhh <22520778@gm.uit.edu.vn>
2024-12-10 14:37:37 +0700

    add components

---
+++ app/Views/teacher/class/record/list.php
@@ -0,0 +1,117 @@
+<link rel="stylesheet" href="<?= base_url(relativePath: 'assets/css/style.css') ?>">
+
+<div class="classlists">
+    <div class="classlists-heading">
+        <?= view('components/heading') ?>
+    </div>
+    <div class="body">
+        <div class="body-left">
+            <?= view('components/sidebar_teacher') ?>
+        </div>
+        <div class="body-right">
+            Học tập / Lớp học / Báo cáo học lực lớp
+            <div class="classlists-tools">
+                <div class="tools">
+                    <?= view('components/filter') ?>
+                    <?= view('components/searchbar') ?>
+                    <?= view('components/dropdown', [
+                        'options' => ['15p (1)', '15p (2)', '45p (1)', '45p (2)', 'Cuối kỳ'],
+                        'dropdown_id' => 'testtype-dropdown',
+                        'name' => 'dropdown',
+                        'selected_text' => 'Chọn bài kiểm tra',
+                        'value' => '',
+                    ]) ?>
+                </div>
+                <div class="tool-add">
+                    <?= view('components/excel_export') ?>
+                </div>
+            </div>
+            <div class="tabless">
+                <?= view('components/tables/teacherRecordList') ?>
+            </div>
+            <?= view('components/pagination') ?>
+        </div>
+    </div>
+</div>
+
+<style>
+    *,
+    *::before,
+    *::after {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    .classlists {
+        display: flex;
+        width: 100%;
+        height: 100%;
+        flex-direction: column;
+        align-items: flex-start;
+        background: #FFF;
+    }
+
+    .classlists-heading {
+        height: 60px;
+        width: 100%;
+    }
+
+    .body {
+        display: flex;
+        align-items: flex-start;
+        flex: 1 0 0;
+        align-self: stretch;
+        background: #F9FAFB;
+        overflow: hidden;
+    }
+
+    .body-left {
+        height: 100%;
+        overflow-y: auto;
+    }
+
+    .body-right {
+        display: flex;
+        padding: 20px;
+        flex-direction: column;
+        align-items: flex-start;
+        gap: 20px;
+        flex: 1 0 0;
+        align-self: stretch;
+        color: #000;
+        font-family: Inter;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+        overflow-y: auto;
+    }
+
+    .classlists-tools {
+        display: flex;
+        padding: 10px;
+        justify-content: space-between;
+        align-items: flex-start;
+        align-self: stretch;
+        border-radius: 10px;
+        background: #FFF;
+    }
+
+    .tools {
+        width: 50%;
+        display: flex;
+        gap: 10px;
+    }
+
+    .tool-add {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+    }
+
+    .tabless {
+        width: 100%;
+        height: 100%;
+    }
+</style>
